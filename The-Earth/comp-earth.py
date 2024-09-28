5905cf2f477691b36900e14664ffb6bdfea3c0d5
Bassma <bsalimane@gmail.com>
2024-09-28 22:54:05 +0100

    add

--- The-Earth/comp-earth.py
+++ The-Earth/comp-earth.py
@@ -1,5 +1,6 @@
 import spiceypy as spice
 import datetime as dt
+import math as math
 
 date_today = dt.dt.today()
 date_today = date_today.strftime("%Y-%m-%dT00:00:00")
@@ -13,3 +14,8 @@ et_today_midnight = spice.utc2et(date_today)
 earth_state_wrt_sun, earth_sun_light_time = spice.spkgeo(targ=earth_id, et=et_today_midnight, 
                                                             ref="ECLIPJ2000", obs=sun_id)
 
+earth_sun_distance = math.sqrt(earth_state_wrt_sun[0] ** 2.0
+                               + earth_state_wrt_sun[1] ** 2.0
+                               + earth_state_wrt_sun[2] ** 2.0)
+
+earth_sun_distance_au = spice.convrt(earth_sun_distance, "km", "au")
