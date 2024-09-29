c343cfa1ebdd47ed3e45b6ff4c9f24505fcfd437
Bassma <bsalimane@gmail.com>
2024-09-29 21:55:47 +0100

    changes

--- The-Earth/comp-earth.py
+++ The-Earth/comp-earth.py
@@ -10,6 +10,7 @@ sun_id = 10
 
 spice.furnsh("../kernels/lsk/naif0012.tls.txt")
 spice.furnsh("../kernels/spk/de432s.bsp")
+spice.furnsh("../kernels/pck/gm_de431.bsp")
 
 et_today_midnight = spice.utc2et(date_today)
 earth_state_wrt_sun, earth_sun_light_time = spice.spkgeo(targ=earth_id, et=et_today_midnight, 
@@ -23,3 +24,8 @@ earth_sun_distance_au = spice.convrt(earth_sun_distance, "km", "au")
 
 earth_state_wrt_sun = np.array(earth_state_wrt_sun)
 earth_sun_distance = np.linage.norm(earth_state_wrt_sun[:3])
+earth_orb_speed_wrt_sun = np.linalg.norm(earth_state_wrt_sun[3:])
+
+_, GM_SUN = spice.bodvcd(bodyid=10, item="GM", maxn=1)
+v_orb_func = lambda gm, r: np.sqrt(gm/r)
+
